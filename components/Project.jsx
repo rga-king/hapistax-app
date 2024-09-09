e04346b651033e217789c86b8cdf0e01e096a37c
rga-king <rga.king@gmail.com>
2024-09-09 23:43:39 +0100

    add multiple testimonials

--- components/Project.jsx
+++ components/Project.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Toolkit from "@/components/Toolkit.jsx";
 import styles from "@/css/Project.module.css";
+import siteStyles from "@/css/Site.module.css";
 
 export default function Project({
   title: { heading, link },
@@ -16,7 +17,7 @@ export default function Project({
       </Link>
       <div className={styles.description}>
         <h3>
-          <Link href={link} className={styles.headingLink} target="_blank">
+          <Link href={link} className={siteStyles.headingLink} target="_blank">
             {heading}
           </Link>
         </h3>
