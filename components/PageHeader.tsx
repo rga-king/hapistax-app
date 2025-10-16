be7f37e29a058aa4249a4cf4e9346571c886632c
rga-king <rga.king@gmail.com>
2025-10-16 15:24:33 +0100

    refactor non-home page header

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import Logo from "@/components/BaseLogo";
 import Mugshot from "@/components/Mugshot";
 import siteStyles from "@/css/Site.module.css";
@@ -7,12 +8,12 @@ import styles from "@/css/Header.module.css";
 export default function HomeHeader() {
   return (
     <header className={`${siteStyles.row} ${siteStyles.logoLink} ${styles.header}`}>
-      <div className={styles.mugshot}>
-        <Mugshot width={48} height={48} />
-      </div>
       <Link href="/">
         <Logo />
       </Link>
+      <div className={styles.mugshot}>
+        <Mugshot width={48} height={48} />
+      </div>
     </header>
   );
 }
